2375cecb09a79881a75861e232b8ab64b338120e
ZLT056 <100818059+ZLT056@users.noreply.github.com>
2022-06-10 09:45:18 +1000

    UPDATED

---
+++ KEMPER FURNITURE/JAVA/PV.js
@@ -0,0 +1,37 @@
+// Get the modal(POPUP)
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("cartbtn");
+
+// Get the <span> element that closes the modal
+
+  var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+
+
